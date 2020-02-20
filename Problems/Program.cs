de8e00c81956898d9e9ad9127471000006afc16e
Carlton Mpofu <G17M7744@ict.ru.ac.za>
2020-02-20 13:04:43 +0200

    all csproj cs and sln

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,23 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        static string Prefix(int input){
+        if(input % 2 == 0){
+            return "Even";
+        }
+        else
+        {
+            return "Odd";
+        }
     }
+    }
+
+    
 }
